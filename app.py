cd99698a045c3a1685296d78a28aea96d7090a90
Monike Freitas <monike.ftsousa@gmail.com>
2023-01-11 13:05:09 -0400

    updates

--- app.py
+++ app.py
@@ -12,5 +12,31 @@ def login():
    return render_template('login.html')
 
 @app.route('/register')
-def login():
+def register():
    return render_template('register.html')
+
+class events:
+    def __init__(self, date, title, desc):
+        self.date = date
+        self.title = title
+        self.desc = desc
+ 
+# creating list
+list = []
+ 
+# appending instances to list
+list.append(events('15h30', 'Levar meu hamster para o jiu jitsu', 'Lorem ipsum dolor si amet'))
+list.append(events('15h30', 'Levar meu hamster para o jiu jitsu', 'Lorem ipsum dolor si amet'))
+list.append(events('15h30', 'Levar meu hamster para o jiu jitsu', 'Lorem ipsum dolor si amet'))
+list.append(events('15h30', 'Levar meu hamster para o jiu jitsu', 'Lorem ipsum dolor si amet'))
+list.append(events('15h30', 'Levar meu hamster para o jiu jitsu', 'Lorem ipsum dolor si amet'))
+list.append(events('15h30', 'Levar meu hamster para o jiu jitsu', 'Lorem ipsum dolor si amet'))
+
+
+@app.route('/daily-view')
+def daily_view():
+    return render_template('daily-view.html', view="D", title="Atividades do dia", events=list)
+
+@app.route('/month-view')
+def month_view():
+   return render_template('month-view.html', view="M", title="Visão mensal")
